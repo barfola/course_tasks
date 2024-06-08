45d18c9b3d8fad381738f2927d9f3e879ad4f82c
barfola <yehonatanbarfy@gmail.com>
2024-06-08 15:37:13 +0300

    file encryption : the file encryption function and file_decryption are done.

--- file_encryption.py
+++ file_encryption.py
@@ -1,4 +1,4 @@
-def get_encrypted_file(file_path, encryption_key=5):
+def file_encryption(file_path, encryption_key=5):
     with open(file_path, 'rb') as file:
         binary_data = bytearray(file.read())
 
@@ -8,4 +8,14 @@ def get_encrypted_file(file_path, encryption_key=5):
         encrypted_file.write(encrypted_binary_data)
 
 
-get_encrypted_file('pic.jpg', 5)
+def file_decryption(file_path, decryption_key=5):
+    with open(file_path, 'rb') as encrypted_file:
+        encrypted_binary_data = bytearray(encrypted_file.read())
+
+    decrypted_binary_data = bytearray(byte ^ decryption_key for byte in encrypted_binary_data)
+
+    with open(file_path, 'wb') as file:
+        file.write(decrypted_binary_data)
+
+
+
